8e66c18696bd4054f595b478b2b42c234654c8c6
Charles Nodot <charles.nodot@insa-lyon.fr>
2020-03-23 17:10:41 +0100

    STEP1BIS

--- sphere/sphere.py
+++ sphere/sphere.py
@@ -4,13 +4,13 @@ class Sphere(object):
 
     def __init__(self, radius):
         # uncomment the following line
-        #self.radius = radius
+        self.radius = radius
         pass
 
     # *** STEP 2 ***
     # uncomment the 2 following lines
-    #def __str__(self):
-    #    return '%s(%s)' % (self.__class__.__name__, self.radius)
+    def __str__(self):
+        return '%s(%s)' % (self.__class__.__name__, self.radius)
 
     def surface(self):
         # *** STEP 3 ***
